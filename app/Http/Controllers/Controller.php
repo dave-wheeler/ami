1ef59050a0da4c5de27ac735ae09ef9092945389
Dave Wheeler <24257875+dave-wheeler@users.noreply.github.com>
2021-04-14 19:15:27 -0600

    Refactor daylight-related controller methods into Controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use AurorasLive\SunCalc;
+use DateTime;
+use Exception;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -61,5 +64,60 @@ class Controller extends BaseController
         }
 
         return $dates;
+    }
+
+    protected function getDaylightAmount(DateTime $date, string $lat, string $lon): int
+    {
+        $times = (new SunCalc($date, $lat, $lon))->getSunTimes();
+
+        $daylight = $times['sunrise']->diff($times['sunset']);
+
+        return $daylight->h * 3600 + $daylight->i * 60 + $daylight->s;
+    }
+
+    protected function getAllDaylightAmounts(array $dates, string $lat, string $lon): array
+    {
+        $result = [
+            'daylight1' => [],
+            'daylight2' => []
+        ];
+
+        /*
+         * Set times to noon to make absolutely sure DST changes
+         * don't result in misinterpretations that break things
+         * */
+        try {
+            $start1 = new DateTime($dates['start1'] . 'T12:00:00');
+            $end1 = new DateTime($dates['end1'] . 'T12:00:00');
+
+            while ($start1 <= $end1) {
+                $result['daylight1'][] = $this->getDaylightAmount($start1, $lat, $lon);
+                $start1->modify('+1 day');
+            }
+        } catch (Exception) {
+        }
+
+        try {
+            $start2 = new DateTime($dates['start2'] . 'T12:00:00');
+            $end2 = new DateTime($dates['end2'] . 'T12:00:00');
+
+            while ($start2 <= $end2) {
+                $result['daylight2'][] = $this->getDaylightAmount($start2, $lat, $lon);
+                $start2->modify('+1 day');
+            }
+        } catch (Exception) {
+        }
+
+        return $result;
+    }
+
+    protected function formattedStringFromSeconds(float $seconds): string
+    {
+        $hours = (int)($seconds / 3600);
+        $seconds -= 3600 * $hours;
+        $minutes = (int)($seconds / 60);
+        $seconds -= 60 * $minutes;
+        $seconds = round($seconds);
+        return "${hours}h ${minutes}m ${seconds}s";
     }
 }
